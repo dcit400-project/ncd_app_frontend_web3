32ebf553ef407c338274b40ad165203dbc81d9e5
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2025-09-08 19:44:12 +0000

    Update server.js

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,4 +1,4 @@
-import server from "../../tts-backend/server.js"; // import the app you exported
+import server from "../../tts_backend/server.js"; // import the app you exported
 import serverless from "serverless-http";
 
 export const handler = serverless(server);
