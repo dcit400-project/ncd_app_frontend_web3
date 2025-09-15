383f584a92f318da59f647695a4e2120254d1130
MarkAryee <102665697+MarkAryee@users.noreply.github.com>
2025-09-15 17:21:49 +0000

    Add files via upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabaseUrl: 'https://ygtiyxkivpjjucfxehbr.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlndGl5eGtpdnBqanVjZnhlaGJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5NDk0NjYsImV4cCI6MjA3MzUyNTQ2Nn0.awc20CTYdnQRQ60hA0kVi36Hc33L91jsnnizQfZGbJo'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://ygtiyxkivpjjucfxehbr.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlndGl5eGtpdnBqanVjZnhlaGJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc5NDk0NjYsImV4cCI6MjA3MzUyNTQ2Nn0.awc20CTYdnQRQ60hA0kVi36Hc33L91jsnnizQfZGbJo'
 };
 
 /*
